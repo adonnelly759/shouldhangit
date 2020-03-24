f297b5c99193e16965e10207521b502d2a0faf05
Aidan <adonnelly759@qub.ac.uk>
2020-03-24 20:46:40 +0000

    fix for static

--- clothes/settings.py
+++ clothes/settings.py
@@ -121,7 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
-STATICFILES_DIRS = (
-  os.path.join(SITE_ROOT, 'static/'),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
